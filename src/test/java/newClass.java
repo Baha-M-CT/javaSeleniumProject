4a502af9c55dcab1d7d628f050b9e0af59ce5db2
dmswlpk <103320248+dmswlpk@users.noreply.github.com>
2022-04-12 11:32:30 -0600

    new branch TatPak added

--- src/test/java/newClass.java
+++ src/test/java/newClass.java
@@ -2,6 +2,7 @@ public class newClass {
     public static void main(String[] args) {
         System.out.println("Hello Selenium");
         System.out.println("commit and push");
+        System.out.println("Test");
 
 
 
