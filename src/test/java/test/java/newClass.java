30c9a3019cea3ad33713c4a6448aa6a15d627f50
Baha Mamet <mametjob@gmail.com>
2022-04-12 13:40:56 -0400

    errors corrected

--- src/test/java/test/java/newClass.java
+++ src/test/java/test/java/newClass.java
@@ -1,9 +1,10 @@
 package test.java;
 
-public class NewClass {
+public class newClass {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Good job!");
     }
 
 
