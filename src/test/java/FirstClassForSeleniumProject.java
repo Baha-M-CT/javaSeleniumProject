cae9e588e307e7c70ac1abbc573e73ff39a8f5f6
Anna Fedorova <snysmymric@gmail.com>
2022-04-15 12:18:58 -0700

    'Added method printStr

---
+++ src/test/java/FirstClassForSeleniumProject.java
@@ -0,0 +1,13 @@
+public class FirstClassForSeleniumProject {
+
+    public static void printStr (String str) {
+        System.out.println("---------------");
+        System.out.println(str);
+        System.out.println("---------------");
+    }
+
+    public static void main(String[] args) {
+        printStr("Anna Fedorova");
+        printStr("First commit");
+    }
+}
