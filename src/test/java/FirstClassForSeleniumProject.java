ce2710cd28b8405c9e0c31b933560fd8a8553a82
Anna Fedorova <snysmymric@gmail.com>
2022-04-15 12:39:48 -0700

    Added method equals, added test1

--- src/test/java/FirstClassForSeleniumProject.java
+++ src/test/java/FirstClassForSeleniumProject.java
@@ -5,9 +5,26 @@ public class FirstClassForSeleniumProject {
         System.out.println(str);
         System.out.println("---------------");
     }
+     public static String printText (String text) {
+        return text;
+     }
+
+
+    public static String equals (String expectedResult, String actualResult) {
+        if (expectedResult.equals(actualResult)) {
+            return "Pass";
+        } else {
+            return "Fail";
+        }
+    }
 
     public static void main(String[] args) {
-        printStr("Anna Fedorova");
-        printStr("First commit");
+
+        /**
+         * Test section
+         */
+        printStr("Test 1");
+        System.out.println(equals("Test 1", printText("Test 1")));
+        System.out.println(equals("Slon",printText("ABC")));
     }
 }
