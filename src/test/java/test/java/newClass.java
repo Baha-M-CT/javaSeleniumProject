0f13377ff7d79d0cd7f45bf527ae8b1fad29ef66
Helen Velichko <helen100289@yandex.ru>
2022-04-16 13:12:42 +0500

    errors corrected

--- src/test/java/test/java/newClass.java
+++ src/test/java/test/java/newClass.java
@@ -5,6 +5,7 @@ public class newClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Good job!");
+        System.out.println("Привет!");
     }
 
 
