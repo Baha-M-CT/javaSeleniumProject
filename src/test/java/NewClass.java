7fa847dd51c7772a5902c74a6165ed4577c57568
Morigor <morozigor@ukr.net>
2022-04-17 20:39:55 +0300

    new branch migor26 been added

---
+++ src/test/java/NewClass.java
@@ -0,0 +1,14 @@
+public class NewClass {
+
+
+    public static boolean check(double a, double b) {
+
+        return a * b > 100;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(check(12.2, 8));
+
+    }
+}
