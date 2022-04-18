d7cc0204ce7d410715e30343c10275d04bd363ce
Anna Fedorova <snysmymric@gmail.com>
2022-04-17 20:39:25 -0700

    Added method getMaxValue, added test for method getMaxValue

--- src/test/java/FirstClassForSeleniumProject.java
+++ src/test/java/FirstClassForSeleniumProject.java
@@ -18,18 +18,38 @@ public class FirstClassForSeleniumProject {
         }
     }
 
+    public static String equals (int expectedResult, int actualResult) {
+        if (expectedResult == actualResult) {
+            return "Pass";
+        } else {
+            return "Fail";
+        }
+    }
+
+    public static int getMaxValue (int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
 
         /**
          * Test section
          */
 
-        /*
-        need add to new method
-         */
         printStr("Test 1");
         System.out.println(equals("Test 1", printText("Test 1")));
         System.out.println(equals("Slon",printText("ABC")));
-        System.out.println("==================");
+
+        printStr("Test 2");
+        int[] arrayInt = new int[]{1,5,7,9,3,2,4,5};
+        System.out.println(equals(9,getMaxValue(arrayInt)));
+
+
     }
 }
