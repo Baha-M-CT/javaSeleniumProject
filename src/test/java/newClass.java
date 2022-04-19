7a9594f759e711d4312db417b55b4d189349e8c5
Nataliia Oliver <psvnatali@gmail.com>
2022-04-19 09:59:30 -0500

    try again onataliia added

--- src/test/java/newClass.java
+++ src/test/java/newClass.java
@@ -2,5 +2,6 @@ public class newClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("New change");
+        System.out.println("Try again");
     }
 }
