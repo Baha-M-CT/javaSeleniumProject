18d50d0c83fd93c633cde61a9364be1f24782296
VladimirFabyshevskiy <vladimirf1981@gmail.com>
2022-04-18 19:16:18 -0600

    new branch lana added

--- src/test/java/newClassBaha.java
+++ src/test/java/newClassBaha.java
@@ -1,6 +1,7 @@
 public class newClassBaha {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String str = "Hello World!";
+        System.out.println(str);
     }
 }
