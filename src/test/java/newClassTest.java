32247fe4da0805f103388f781a31faadb0147be2
Svetlana Dolgopolova <svetnt75@gmail.com>
2022-04-20 19:07:25 +0500

    new branch DSvetlana added

--- src/test/java/newClassTest.java
+++ src/test/java/newClassTest.java
@@ -1,5 +1,8 @@
 public class newClassTest {
     public static void main(String[] args) {
         System.out.println("Hello!!");
+        System.out.println("Git не такой уж сложный");
     }
+
+
 }
