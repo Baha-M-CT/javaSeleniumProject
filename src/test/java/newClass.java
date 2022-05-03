96c56aec271a28096df9d3f2e2476235190c3cd6
Irene Snitko <IreneSnitko@gmail.com>
2022-05-03 15:43:43 +0300

    a new branch sn_irene added

--- src/test/java/newClass.java
+++ src/test/java/newClass.java
@@ -1,8 +1,9 @@
 public class newClass {
 
     public static void main(String[] args) {
-        System.out.println("TestInSelenium");
-        System.out.println("TestInSelenium");
+        System.out.println("TestInSelenium_1");
+        System.out.println("TestInSelenium_2");
+        System.out.println("TestInSelenium_3");
     }
 
 }
