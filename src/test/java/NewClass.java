4fea075bb238f3f88bc0c68340e954dcbd83b41d
Tatjanas1 <tatiana.svistunova1@googlemail.com>
2022-05-04 20:38:20 +0300

    my 3 commit to Baha

--- src/test/java/NewClass.java
+++ src/test/java/NewClass.java
@@ -2,5 +2,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hallo world!");
         System.out.println("Hallo!");
+        System.out.println("world!");
     }
 }
